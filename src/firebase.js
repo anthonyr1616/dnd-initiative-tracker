f5cc4c086f0b322c230c87bdb8ac2e9c67764785
Anthonyr1616 <anthonyr1616@gmail.com>
2026-05-05 00:01:35 -0400

    Use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwlMC2m9lowol9YdJAqIPXVF5K18i-lZQ",
-  authDomain: "dnd-initiative-tracker-dd214.firebaseapp.com",
-  projectId: "dnd-initiative-tracker-dd214",
-  storageBucket: "dnd-initiative-tracker-dd214.firebasestorage.app",
-  messagingSenderId: "378083788843",
-  appId: "1:378083788843:web:496657bba2d143ee04826e",
-  measurementId: "G-Q9CK749MC9",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
